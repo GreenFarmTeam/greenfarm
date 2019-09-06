0e98b73c17aaed103fd7a7c17de578c6a1e7dd5a
16201535wangshenyu <2697431730@qq.com>
2019-09-06 16:14:41 +0800

    项目框架搭建

---
+++ src/main/java/com/nchu/ruanko/greenfarm/GreenfarmApplication.java
@@ -0,0 +1,13 @@
+package com.nchu.ruanko.greenfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenfarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenfarmApplication.class, args);
+    }
+
+}
