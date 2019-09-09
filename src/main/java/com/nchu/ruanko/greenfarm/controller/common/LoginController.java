7cee3f340450e2ddb611def947fed52624bfea3c
yysCyber <2161624113@qq.com>
2019-09-09 20:08:09 +0800

    重置

--- src/main/java/com/nchu/ruanko/greenfarm/controller/common/LoginController.java
+++ src/main/java/com/nchu/ruanko/greenfarm/controller/common/LoginController.java
@@ -1,14 +1,8 @@
 package com.nchu.ruanko.greenfarm.controller.common;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
-    @GetMapping(value = "/password/login.htm")
-    public String passwordLoginPage() {
-        return "common/login.html";
-    }
 
 }
