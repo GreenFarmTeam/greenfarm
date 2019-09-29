a0276a633f91e6c2804eccccbe0cda109bc06297
yysCyber <2161624113@qq.com>
2019-09-29 20:11:12 +0800

    修正

--- src/main/resources/static/js/management/business/farm-review.js
+++ src/main/resources/static/js/management/business/farm-review.js
@@ -1,17 +1,19 @@
 $("a[name='gf_location']").on('click', function () {
-    layer.closeAll();
     var lng = $(this).attr("data-lng");
     var lat = $(this).attr("data-lat");
     var content = "<!DOCTYPE html>" +
         "<html>" +
         "<head>" +
-        "<script type='text/javascript' src='/gfstatic/webjars/jquery/3.4.1/jquery.min.js'></script>" +
-        "<script type='text/javascript' src='/gfstatic/templateone/res/layui/layui.all.js'></script>" +
-        "<script type='text/javascript' src='https://webapi.amap.com/maps?v=1.4.15&key=f48fcde49f6822707df87e6e395b4d08'></script>" +
-        "<script type='text/javascript' src='/gfstatic/js/common/map.js'></script>" +
+        "<meta charset='UTF-8'>" +
         "</head>" +
         "<body>" +
         "<div id='container' style='width: 600px; height: 450px'></div>" +
+        "<script type='text/javascript'>" +
+        "var map = new AMap.Map('container', {" +
+        "zoom : 14," +
+        "resizeEnable : false});" +
+        "markPoint(map, " + lng + ", " + lat + ");" +
+        "</script>" +
         "</body>" +
         "</html>";
     layer.open({
