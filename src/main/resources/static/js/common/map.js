5476749a7e6420e3e6f6bd91be19f03f6bbc4a4f
S16206118 <1786962831@qq.com>
2019-10-18 13:12:56 +0800

    修复地图BUG

--- src/main/resources/static/js/common/map.js
+++ src/main/resources/static/js/common/map.js
@@ -105,47 +105,16 @@ function initMap2(containerId) {
         $("#now-lat").val(data.position.lat);
     }
     //解析定位错误信息
-    function onError(data) {
-
-    }
+    function onError(data) {}
 
     return map;
 
 }
 
-var map = new AMap.Map('show_map', {
-    resizeEnable : true,
-    zoom : 15
-});
-markPoint(map, $("#gf_lng").val(), $("#gf_lat").val());
+// var map = new AMap.Map('container', {
+//     resizeEnable : true,
+//     zoom : 15
+// });
+// markPoint(map, $("#gf_lng").val(), $("#gf_lat").val());
 
-$("button[name='gf_location']").on('click', function () {
-    var lng = $(this).attr("data-lng");
-    var lat = $(this).attr("data-lat");
-    var content = "<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-        "<meta charset='UTF-8'>" +
-        "</head>" +
-        "<body>" +
-        "<div id='container' style='width: 800px; height: 600px'></div>" +
-        "<script type='text/javascript'>" +
-        "var map = initMap2('container');" +
-        "map.setCenter(new AMap.LngLat(" + lng + ", " + lat + "));" +
-        "var localLng = $('#now-lng').val();" +
-        "var localLat = $('#now-lat').val();" +
-        "var driving = new AMap.Driving({" +
-        "map:map" +
-        "});" +
-        "driving.search(new AMap.LngLat(localLng, localLat), new AMap.LngLat(" + lng + ", " + lat + "), function(status, result) {});" +
-        "</script>" +
-        "</body>" +
-        "<ml>";
-    layer.open({
-        type: 1,
-        skin: 'layui-layer-rim',
-        area: ['800px', '600px'],
-        shadeClose: false,
-        content: content
-    });
-});
+
